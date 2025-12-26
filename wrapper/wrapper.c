a4ec44bdf5a8657b3a36623765e8aff46ce3d692
Bratescu Andrei-Seren <121115397+joey121982@users.noreply.github.com>
2025-12-26 07:44:17 +0200

    update wrapper.c

--- wrapper/wrapper.c
+++ wrapper/wrapper.c
@@ -1,23 +1,18 @@
-/* wrapper.c */
 #include <stddef.h>
-#include "allocator.h" // Ensures we see your function declarations
+#include "allocator.h"
 
-// Standard malloc signature
 void* malloc(size_t size) {
     return allocate(size);
 }
 
-// Standard free signature
 void free(void* ptr) {
     freemem(ptr);
 }
 
-// Standard realloc signature
 void* realloc(void* ptr, size_t size) {
     return reallocate(ptr, size);
 }
 
-// Standard calloc signature
 void* calloc(size_t nmemb, size_t size) {
     return callocate(nmemb, size);
 }
